b3609d7509bba507e421f09caec238e3c9e0e772
zainmustafa <zmdhillo@gmail.com>
2018-07-12 13:17:18 +0500

    add netflix constants

--- app/scripts/constants/index.js
+++ app/scripts/constants/index.js
@@ -24,6 +24,9 @@ export const ActionTypes: Object = keyMirror({
     FETCH_POPULAR_REPOS_SUCCESS: undefined,
     FETCH_POPULAR_REPOS_FAILURE: undefined,
     FETCH_POPULAR_REPOS_CANCEL: undefined,
+    ADD_TO_PLAYLIST_REQUEST: undefined,
+    ADD_TO_PLAYLIST_SUCCESS: undefined,
+    ADD_TO_PLAYLIST_FAILURE: undefined,
     CANCEL_FETCH: undefined,
     SHOW_ALERT: undefined,
     HIDE_ALERT: undefined
